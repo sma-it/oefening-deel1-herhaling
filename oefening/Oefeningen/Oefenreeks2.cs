2fe7657f466c8d4afa9fa23b590463e6165ab751
yvan vander sanden <yvan@attr-x.net>
2018-10-28 21:40:19 +0200

    Oefenreeks 2 toegevoegd

---
+++ oefening/Oefeningen/Oefenreeks2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+	public class Oefenreeks2
+	{
+		/* 1: zorg dat deze functie als resultaat het eerste veelvoud geeft
+		 *	  van het argument, dat groter dan, of gelijk is aan 100.
+		 */
+		public static int Maximize(int number)
+		{
+			return 0;
+		}
+
+		/* 2: Maak een tweede functie 'Maximize' met een extra getal als argument.
+		 *    Nu bepaalt dit getal hoe groot het resultaat minstens moet zijn.
+		 */
+
+
+
+		/* 3: Deze functie geeft als resultaat het eerste fibonaccigetal, groter dan
+		 *    het argument.
+		 */
+
+		public static int FibonacciOfAtLeast(int number)
+		{
+			return 0;
+		}
+
+		/* 4: Geef als resultaat een string met de eerste vier tekens van het 
+		 *    argument.
+		 */
+
+		public static string Start(string word)
+		{
+			return string.Empty;
+		}
+
+		/* 5: Geef als resultaat een string met het begin van de het argument.
+		 *    Het aantal tekens in het resultaat wordt bepaald door het tweede argument.
+		 */
+
+		public static string Start(string word, int num)
+		{
+			return string.Empty;
+		}
+
+		/* 6: Deze functie geeft als resultaat een array van strings. Je maakt die
+		 *    array door het argument (een zin) te splitsen in woorden.
+		 */
+
+		public static string[] Split(string sentence)
+		{
+			return new string[] { };
+		}
+
+		/* 7: Deze functie krijgt als input een zin. Vervang alle spaties door 
+		 *    een puntkomma en geef de nieuwe versie als functieresultaat.
+		 */
+
+		public static string ReplaceSpaces(string sentence)
+		{
+			return string.Empty;
+		}
+
+		/* 8: Deze functie moet controleren of de string start met de inhoud van
+		 *    het tweede argument.
+		 */
+
+		public static bool StartsWith(string word, string start)
+		{
+			return false;
+		}
+
+		/* 9: Deze functie moet elke letter in een zin met een teken verhogen.
+		 *		Een a wordt een b, een b een c enzovoort. Spaties en leestekens blijven
+		 *		wel behouden, en een z wordt een a. Er komen geen hoofdletters voor in de
+		 *		zin.
+		 */
+
+		public static string Encrypt(string zin)
+		{
+			return string.Empty;
+		}
+
+		/* 10: Deze functie doet het omgekeerde dan de vorige: je verlaagt elk teken met 1.
+		 *     (Met andere woorden: een zin die met de vorige functie gecodeerd is, kan je
+		 *     nu terug ontcijferen.)
+		 */
+
+		public static string Decrypt(string zin)
+		{
+			return string.Empty;
+		}
+	}
+}
--- oefening/Program.cs
+++ oefening/Program.cs
@@ -14,7 +14,8 @@ namespace First
 		{
 			var menu = new Utils.Menu();
 			// voeg oefeningen to door een callback naar een functie
-			menu.AddOption('1', "Voer Oef1 uit", DoOef1);
+			menu.AddOption('1', "Voer Oefenreeks 1 uit", DoOef1);
+			menu.AddOption('2', "Voer Oefenreeks 2 uit", DoOef2);
 
 			menu.Start();
 		}
@@ -186,6 +187,94 @@ namespace First
 				Console.WriteLine("10: Increment(int[], int) niet gevonden");
 			}
 		}
+
+		static void DoOef2()
+		{
+			// object setup
+			Utils.Object Obj = null;
+			if (Utils.Object.DoesClassExist("First.Oefenreeks2"))
+			{
+				Obj = new Utils.Object("First.Oefenreeks2");
+			}
+			else
+			{
+				Console.WriteLine("De class Oefenreeks2 bestaat niet meer. Controleer je code op fouten.");
+				return;
+			}
+
+			// oefening 1
+			Console.Write("1: \t\t108");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.Maximize(12));
+			Console.Write("1: \t\t102");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.Maximize(3));
+
+			// oefening 2
+			if (Obj.HasMethod("Maximize", new Type[] { typeof(int), typeof(int) }))
+			{
+				Console.Write("2: \t\t60");
+				Console.WriteLine("\t\t\t" + Obj.Method("Maximize", new Type[] { typeof(int), typeof(int) }).Invoke(new object[] { 12, 50 }).ToString());
+
+				Console.Write("2: \t\t204");
+				Console.WriteLine("\t\t\t" + Obj.Method("Maximize", new Type[] { typeof(int), typeof(int) }).Invoke(new object[] { 12, 200 }).ToString());
+			}
+			else
+			{
+				Console.WriteLine("2: Maximize(int , int) niet gevonden");
+			}
+
+			// oefening 3
+			Console.Write("3: \t\t13");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.FibonacciOfAtLeast(10).ToString());
+			Console.Write("3: \t\t6765");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.FibonacciOfAtLeast(6000).ToString());
+
+			// oefening 4
+			Console.Write("4: \t\tabcd");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.Start("abcdefghijklml"));
+			Console.Write("4: \t\t!$@*");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.Start("!$@*^#$*@$%^&"));
+
+			// oefening 5
+			Console.Write("5: \t\t!$@");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.Start("!$@*^#$*@$%^&", 3));
+			Console.Write("5: \t\t!$@*^");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.Start("!$@*^#$*@$%^&", 5));
+
+			// oefening 6
+			string[] result = Oefenreeks2.Split("wie (code) schrijft die blijft");
+			string[] test = new string[] { "wie", "(code)", "schrijft", "die", "blijft" };
+			for (int i = 0; i < test.Length; i++) {
+				Console.Write("6:\t\t" + test[i]);
+				if(result.Length > i)
+				{
+					Console.WriteLine("\t\t\t" + result[i]);
+				} else
+				{
+					Console.WriteLine("");
+				}
+			}
+
+			// oefening 7
+			Console.WriteLine("7  : You;are;the;semicolon;to;my;statements");
+			Console.WriteLine("you: " + Oefenreeks2.ReplaceSpaces("You are the semicolon to my statements"));
+
+			// oefening 8
+			Console.Write("8: \t\tTrue");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.StartsWith("this is correct", "this").ToString());
+			Console.Write("8: \t\tFalse");
+			Console.WriteLine("\t\t\t" + Oefenreeks2.StartsWith("this is not correct", "not").ToString());
+
+			// oefening 9
+			Console.WriteLine("9:       'zero and null are not the same.'");
+			Console.WriteLine("Becomes: 'afsp boe ovmm bsf opu uif tbnf.'");
+			Console.WriteLine("You    : '" + Oefenreeks2.Encrypt("zero and null are not the same.") + "'");
+
+			// oefening 10
+			Console.WriteLine("10:      'afsp boe ovmm bsf opu uif tbnf.'");
+			Console.WriteLine("Becomes: 'zero and null are not the same.'");
+			Console.WriteLine("You    : '" + Oefenreeks2.Decrypt("afsp boe ovmm bsf opu uif tbnf.") + "'");
+		}
+
 
 	}
 }
